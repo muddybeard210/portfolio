0670208d98c760d35fb8160f545601df3fc3cd0b
Matthew Smith <mattsmith203@hotmail.com>
2016-03-06 09:31:04 -0800

    added some article clone

---
+++ projects.js
@@ -0,0 +1,9 @@
+var rawData = [
+  {
+    title: 'POMPEII',
+    description: 'Side scrolling game.  Defend the city of Pompeii',
+    url: 'http://andrewpeterwatson.github.io/Swift',
+    img: 'imgs/pompeii.png',
+    publishedOn: '02-29-2016'
+  },
+];
